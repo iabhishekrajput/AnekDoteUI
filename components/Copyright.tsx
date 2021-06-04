2bad931d15b44d7c89d7e146609b3a9d000ef65e
Abhishek Singh <iabhishekrajput@gmail.com>
2021-06-04 22:32:44 +0530

    Modified Home Page

--- components/Copyright.tsx
+++ components/Copyright.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import MuiLink from "@material-ui/core/Link";
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" color="textSecondary">
       {"Copyright © "}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        AnekDote
-      </MuiLink>{" "}
+      AnekDote
       {new Date().getFullYear()}
       {"."}
     </Typography>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { Container, Typography, Box } from "@material-ui/core";
+import { Container, Typography, Box, makeStyles } from "@material-ui/core";
 
 import Copyright from "../components/Copyright";
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  footer: {
+    padding: "1rem",
+    position: "fixed",
+    bottom: 0,
+  },
+});
+
 export default function Index() {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth="md">
-      <Box my={4}>
-        <Typography>Hello World</Typography>
+    <Container maxWidth="md" className={classes.root}>
+      <Box className={classes.container} my={4}>
+        <Typography variant="h4">AnekDote</Typography>
+      </Box>
+      <Box className={classes.footer}>
         <Copyright />
       </Box>
     </Container>
