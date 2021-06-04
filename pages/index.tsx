c0b88d61449d397e5535447e45ccae9dff81d5e5
Abhishek Singh <iabhishekrajput@gmail.com>
2021-06-04 22:11:49 +0530

    Changed HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { Container, Typography, Box } from "@material-ui/core";
 
-import ProTip from "../components/ProTip";
-import Link from "../components/Link";
 import Copyright from "../components/Copyright";
 
 export default function Index() {
   return (
     <Container maxWidth="md">
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
+        <Typography>Hello World</Typography>
         <Copyright />
       </Box>
     </Container>
